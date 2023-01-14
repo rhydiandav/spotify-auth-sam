443624fafa451ff3ad15ec635e6c8e04d70234e0
Rhydian Davies <rhydian@Rhydians-MacBook-Air.local>
2023-01-14 14:48:43 +0000

    add test to ensure parameters are included with request

--- src/__tests__/app.test.ts
+++ src/__tests__/app.test.ts
@@ -1,13 +1,27 @@
 const app = require("../app");
 
+const testClientID = "123";
+const testRedirectURI = "http://redirect.uri";
+const testScope = "scope";
+
 describe("login", function () {
+  const defaultEnv = process.env;
+
+  beforeEach(() => {
+    process.env = { ...defaultEnv };
+  });
+
   it("redirects to spotify login with the necessary parameters", async () => {
+    process.env.CLIENT_ID = testClientID;
+    process.env.REDIRECT_URI = testRedirectURI;
+    process.env.SCOPE = testScope;
+
     const result = await app.login();
 
     expect(result.statusCode).toEqual(302);
 
     expect(result.headers.Location).toEqual(
-      "https://accounts.spotify.com/authorize?"
+      `https://accounts.spotify.com/authorize?client_id=${testClientID}&response_type=code&redirect_uri=${testRedirectURI}&scope=${testScope}`
     );
   });
 });
