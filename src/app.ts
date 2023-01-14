8d324db88001ba2aea6501c8aabb1048af1ecb5a
Rhydian Davies <rhydian@Rhydians-MacBook-Air.local>
2023-01-14 14:50:35 +0000

    include params in request

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 exports.login = async () => {
+  const { CLIENT_ID, REDIRECT_URI, SCOPE } = process.env;
+
   try {
     return {
       statusCode: 302,
       headers: {
-        Location: "https://accounts.spotify.com/authorize?",
+        Location: `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${REDIRECT_URI}&scope=${SCOPE}`,
       },
     };
   } catch (err) {
