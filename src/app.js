950ca3dbbd20eb8a4af2cc2c638f8f26fee6855d
Rhydian Davies <rhydian@Rhydians-MacBook-Air.local>
2023-01-14 12:16:17 +0000

    tidy up initial template

---
+++ src/app.js
@@ -0,0 +1,13 @@
+exports.lambdaHandler = async () => {
+  try {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "hello world",
+      }),
+    };
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
+};
---
+++ src/tests/app.test.js
@@ -0,0 +1,16 @@
+const app = require("../app.js");
+const chai = require("chai");
+const expect = chai.expect;
+var event, context;
+
+describe("Tests index", function () {
+  it("verifies successful response", async () => {
+    const result = await app.lambdaHandler(event, context);
+
+    expect(result.statusCode).to.equal(200);
+
+    let response = JSON.parse(result.body);
+
+    expect(response.message).to.be.equal("hello world");
+  });
+});
